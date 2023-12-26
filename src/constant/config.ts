62fe41bd4fcecc03d37a776570cc00a203ea4e38
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2023-12-26 19:51:00 +0700

    Push Code

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: 'Radar',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'Radar Network is a dynamic and forward-thinking media platform dedicated to bringing the realm of Decentralized Finance (DeFi) to the masses.',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'https://tsnext-tw.thcl.dev',
 };
